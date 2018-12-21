94548bc949f4e0381f2583caa8e8375cbcbc0b16
duongtri <43365774+duongtri@users.noreply.github.com>
2018-12-21 17:13:46 +0700

    Update IReceiver.java

--- IReceiver.java
+++ IReceiver.java
@@ -1,8 +1,14 @@
 /*
-* Interface IReceiver to send sum step
-* */
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author tri duong
+ */
 public interface IReceiver {
-
-    void sendMessage(String mString);
-
+    public void send(String msg);
+    
 }
